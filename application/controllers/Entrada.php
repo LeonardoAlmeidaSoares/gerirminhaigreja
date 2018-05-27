39c4969ce832bac9fba5b65c43c55f204792c9b9
Leonardo Soares <leonardoalmeidasoares23@gmail.com>
2018-05-27 16:52:05 -0300

    Correção de alteração de membros

--- application/controllers/Entrada.php
+++ application/controllers/Entrada.php
@@ -1,26 +1,26 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Entrada extends CI_Controller {
-	
-	public function listar(){
-		session_start();
+
+    public function listar() {
+        session_start();
         if (!isset($_SESSION["logged"])) {
             header("location:" . base_url("index.php/login"));
         }
-		$this->load->model("ModelEntrada");
-		$parametros = array(
-			"entradas" => $this->ModelEntrada->getEntradas(),
-		);
-		
-		$this->load->model("Modelmembro");		
-		$parametrosnavbar = array(
-			"membros_carteirinha" => $this->Modelmembro->getMembros()
-		);
-		$this->load->view('header');
-		$this->load->view('navbar',$parametrosnavbar);
-		$this->load->view('menu');
-		$this->load->view('entrada/listarentradas',$parametros);
-	}
+        $this->load->model("ModelEntrada");
+        $parametros = array(
+            "entradas" => $this->ModelEntrada->getEntradas(),
+        );
 
-	
+        $this->load->model("Modelmembro");
+        $parametrosnavbar = array(
+            "membros_carteirinha" => $this->Modelmembro->getMembros()
+        );
+        $this->load->view('header');
+        $this->load->view('navbar', $parametrosnavbar);
+        $this->load->view('menu');
+        $this->load->view('entrada/listarentradas', $parametros);
+    }
+
 }
